72f49ab99af8712157ab461e94ae059e4f9d071e
Alexandre Altgauzen <altgauzen@gmail.com>
2021-06-16 21:26:55 +0100

    Identaçoes 1 a 4 em andamento

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-
-
 function alerta() {
-
-    let emailInserido = document.getElementById('campoEmail').value;
-    let senhaInserida = document.getElementById('campoSenha').value;
-    let emailCerto = 'tryber@teste.com' 
-    let senhaCerta = '123456'
-   
+  let emailInserido = document.getElementById('campoEmail').value;
+  let senhaInserida = document.getElementById('campoSenha').value;
+  let emailCerto = 'tryber@teste.com' 
+  let senhaCerta = '123456'
     if ( emailInserido === emailCerto && senhaInserida === senhaCerta ) {
-            alert('Olá, Tryber!')
-
+        alert('Olá, Tryber!');
     } else {
         alert('Login ou senha inválidos.');
-
     }
-    
-
 }
